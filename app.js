5d60a9050ce56762a3b9ac5aae590ed0559810f3
MrBlackGhostt <hemantkumar4213@icloud.com>
2023-02-10 11:00:39 +0530

    the link of mongoose change from

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-const express = require('express');
-const app = express();
-const path = require('path')
-const mongoose = require('mongoose')
-const CampGround = require('./models/campground.js')
 
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/campGround', {useNewUrlParser:true})
-.then(data=>
-    console.log('CONNECT'))
-    .catch(err=>{
-        console.log(err);
-    })
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
+
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+// const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+mongoose.set('strictQuery',true);
+
+mongoose.connect('mongodb://https://0.0.0.0:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log(adauhdah);
+}).catch(err => {
+    console.log("Error, MONGO CONNECTION!!!!")
+    console.log(err)
+});
+const app = express();
+
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req,res)=>{
+app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
 app.get('/campground', async(req,res)=>{
-    const camp = new CampGround({title:'first capm ground'})
+    const camp = new Campground({title:'first capm ground'})
     await camp.save();
     res.send(camp);
 })
